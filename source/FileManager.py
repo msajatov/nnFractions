811ae719aa3fa5925e3c044761a38485bc66422d
msajatov <e1125768@student.tuwien.ac.at>
2019-03-20 15:18:55 +0100

    Restructure run_model.py input arguments and file path handling using a more powerful FileManager (doesn't work for datacard tool because the use_config path must be located below the path of run_model (?))

--- source/FileManager.py
+++ source/FileManager.py
@@ -2,27 +2,116 @@ import os
 
 class FileManager():
 
-    def __init__(self, outputpath = ""):
+    def __init__(self, outputpath=""):
         self.outputpath = outputpath
-        self.model_dir = "{0}/models/default/".format(outputpath)
-        self.model_name = "empty"
-        self.scaler_name = "empty"
+        self.model_dirname = "models/default"
+        self.model_dirpath = "{0}/{1}".format(self.outputpath, self.model_dirname)
+        self.model_filename = "empty"
+        self.model_filepath = "empty"
+        self.scaler_dirname = "models/default"
+        self.scaler_dirpath = "{0}/{1}".format(self.outputpath, self.model_dirname)
+        self.scaler_filename = "empty"
+        self.scaler_filepath = "empty"
+        self.prediction_dirname = "predictions/default"
+        self.prediction_dirpath = "{0}/{1}/".format(self.outputpath, self.prediction_dirname)
+        self.datacard_config_dirname = "datacard/conf/default"
+        self.datacard_config_dirpath = "{0}/{1}".format(self.outputpath, self.datacard_config_dirname)
+        self.datacard_dirname = "datacard/default"
+        self.datacard_dirpath = "{0}/{1}".format(self.outputpath, self.datacard_dirname)
+        self.datacard_plot_dirname = "datacard/plot/default"
+        self.datacard_plot_dirpath = "{0}/{1}".format(self.outputpath, self.datacard_plot_dirname)
 
-    def set_model_name(self, name):
-        self.model_name = name
+        self.fractions_filepath = ""
 
-    def set_scaler_name(self, name):
-        self.scaler_name = name
+    def get_output_root_path(self):
+        return self.outputpath
 
-    def set_model_dir(self, dirname):
-        self.model_dir = "{0}/models/{1}/".format(self.outputpath, dirname)
+    def set_model_filename(self, name):
+        self.model_filename = name
+        self.model_filepath = "{0}/{1}".format(self.model_dirpath, self.model_filename)
 
-    def get_model_dir(self):
-        return self.model_dir
+    def set_scaler_filename(self, name):
+        self.scaler_filename = name
+        self.scaler_filepath = "{0}/{1}".format(self.scaler_dirpath, self.scaler_filename)
 
-    def get_model_path(self):
-        return "{0}/{1}".format(self.model_dir, self.model_name)
+    def set_model_dirname(self, model_dir):
+        # used as root dir for both model and scaler
+        self.model_dirname = model_dir
+        self.model_dirpath = "{0}/{1}".format(self.outputpath, self.model_dirname)
 
-    def get_scaler_path(self):
-        return "{0}/{1}".format(self.model_dir, self.scaler_name)
+        self.scaler_dirname = model_dir
+        self.scaler_dirpath = "{0}/{1}".format(self.outputpath, self.model_dirname)
+
+        if not os.path.exists(self.model_dirpath):
+            os.makedirs(self.model_dirpath)
+
+    def get_model_dirname(self):
+        return self.model_dirname
+
+    def get_model_dirpath(self):
+        return self.model_dirpath
+
+    def get_model_filename(self):
+        return self.model_filename
+
+    def get_model_filepath(self):
+        return self.model_filepath
+
+    def get_scaler_filename(self):
+        return self.scaler_filename
+
+    def get_scaler_filepath(self):
+        return self.scaler_filepath
+
+    def set_prediction_dirname(self, prediction_dir):
+        self.prediction_dirname = prediction_dir
+        self.prediction_dirpath = "{0}/{1}".format(self.outputpath, self.prediction_dirname)
+        if not os.path.exists(self.prediction_dirpath):
+            os.makedirs(self.prediction_dirpath)
+
+    def get_prediction_dirpath(self):
+        return self.prediction_dirpath
+
+    def set_datacard_config_dirname(self, datacard_config_dir):
+        self.datacard_config_dirname = datacard_config_dir
+        self.datacard_config_dirpath = "{0}/{1}".format(self.outputpath, self.datacard_config_dirname)
+        if not os.path.exists(self.datacard_config_dirpath):
+            print "Warning: Datacard config dir does not exist ({0})".format(self.datacard_config_dirpath)
+
+    def get_datacard_config_dirpath(self):
+        return self.datacard_config_dirpath
+
+    def get_datacard_config_dirname(self):
+        return self.datacard_config_dirname
+
+    def set_datacard_output_dirname(self, datacard_dir):
+        self.datacard_dirname = datacard_dir
+        self.datacard_dirpath = "{0}/{1}".format(self.outputpath, self.datacard_dirname)
+        if not os.path.exists(self.datacard_dirpath):
+            print "Warning: Datacard config dir does not exist ({0})".format(self.datacard_dirpath)
+
+    def get_datacard_output_dirpath(self):
+        return self.datacard_dirpath
+
+    def get_datacard_output_dirname(self):
+        return self.datacard_dirname
+
+    def set_datacard_plot_dirname(self, datacard_dir):
+        self.datacard_plot_dirname = datacard_dir
+        self.datacard_plot_dirpath = "{0}/{1}".format(self.outputpath, self.datacard_plot_dirname)
+        if not os.path.exists(self.datacard_plot_dirpath):
+            print "Warning: Datacard config dir does not exist ({0})".format(self.datacard_plot_dirpath)
+
+    def get_datacard_plot_dirpath(self):
+        return self.datacard_plot_dirpath
+
+    def get_datacard_plot_dirname(self):
+        return self.datacard_plot_dirname
+
+    def set_fractions_filepath(self, path):
+        self.fractions_filepath = path
+
+    def get_fractions_filepath(self):
+        return self.get_fractions_filepath()
+
 
--- source/run_model.py
+++ source/run_model.py
@@ -11,13 +11,13 @@ from FileManager import FileManager
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', dest='channel', help='Decay channel' ,choices = ['mt','et','tt','em'], default = 'mt')
-    parser.add_argument('-m', dest='model',   help='ML model to use' ,choices = ['keras','xgb'],  default = 'keras')
-    parser.add_argument('-t', dest='train',   help='Train new model' , action='store_true')
-    parser.add_argument('-s', dest='short',   help='Do !!NOT!! predict shapes' , action='store_true')
-    parser.add_argument('-d', dest='datacard',  help='Only produce Datacard' , action='store_true')
-    parser.add_argument('-e', dest='era',  help='Era' , choices=["2016","2017"], required = True)
-    parser.add_argument('--add_nominal', dest='add_nom',  help='Add nominal samples to prediction', action='store_true' )
+    parser.add_argument('-c', dest='channel', help='Decay channel' ,choices = ['mt', 'et', 'tt', 'em'], default='mt')
+    parser.add_argument('-m', dest='model',   help='ML model to use', choices=['keras', 'xgb'],  default='keras')
+    parser.add_argument('-t', dest='train',   help='Train new model', action='store_true')
+    parser.add_argument('-s', dest='shapes',   help='Predict shapes', action='store_true')
+    parser.add_argument('-p', dest='predict', help='Make prediction', action='store_true')
+    parser.add_argument('-e', dest='era',  help='Era', choices=["2016", "2017"], required = True)
+    parser.add_argument('--add_nominal', dest='add_nom', help='Add nominal samples to prediction', action='store_true')
     args = parser.parse_args()
 
     print "---------------------------"
@@ -26,22 +26,22 @@ def main():
     print "Using {0}".format(args.model), keras.__version__
     if args.train:
         print "Training new model"
-    if args.short:
+    if not args.shapes:
         print "Not predicting shape templates."
     print "---------------------------"
-
         
-    run(samples = "conf/frac_config_{0}_{1}.json".format(args.channel,args.era),
+    run(samples = "conf/frac_config_{0}_{1}.json".format(args.channel, args.era),
         channel=args.channel,
-        era = args.era,
-        use = args.model,
-        train = args.train,
-        short = args.short,
-        datacard = args.datacard,
-        add_nominal = args.add_nom
-          )
+        era=args.era,
+        use=args.model,
+        train=args.train,
+        shapes=args.shapes,
+        predict=args.predict,
+        add_nominal=args.add_nom
+        )
 
-def run(samples,channel, era, use, train,short, datacard = False, add_nominal=False ):
+
+def run(samples, channel, era, use, train=False, shapes=False, predict=False, add_nominal=False):
 
     if use == "xgb":
         from XGBModel import XGBObject as modelObject
@@ -51,7 +51,6 @@ def run(samples,channel, era, use, train,short, datacard = False, add_nominal=Fa
         from KerasModel import KerasObject as modelObject
         parameters = "conf/parameters_keras.json"
 
-
     read = Reader(channel = channel,
                   config_file = samples,
                   folds=2,
@@ -60,18 +59,23 @@ def run(samples,channel, era, use, train,short, datacard = False, add_nominal=Fa
     target_names = read.config["target_names"]
     variables = read.config["variables"]
 
-    model_dir = era
+    model_dir = "models_test/" + era
     model_name = "{0}.{1}".format(channel, use)
 
     file_manager = FileManager("/afs/hephy.at/work/m/msajatovic/CMSSW_9_4_0/src/dev/nnFractions/output")
 
-    file_manager.set_model_dir(model_dir)
-    file_manager.set_model_name(model_name)
-
-    if not os.path.exists(file_manager.get_model_dir()):
-        os.makedirs(file_manager.get_model_dir())
+    file_manager.set_model_dirname(model_dir)
+    file_manager.set_model_filename(model_name)
 
     scaler = None
+
+    file_manager.set_scaler_filename("StandardScaler.{0}.pkl".format(channel))
+
+    print "debug:" + "\n"
+    print file_manager.get_model_dirpath() + "\n"
+    print file_manager.get_model_filepath() + "\n"
+    print file_manager.get_model_dirname() + "\n"
+    print file_manager.get_model_filename() + "\n"
 
     if train:
         print "Training new model"
@@ -79,70 +83,63 @@ def run(samples,channel, era, use, train,short, datacard = False, add_nominal=Fa
         trainSet = read.getSamplesForTraining()
 
         print "Fit Scaler to training set...",
-        scaler = trainScaler(trainSet, variables )
+        scaler = trainScaler(trainSet, variables)
 
         print " done. Dumping for later."
-        file_manager.set_scaler_name("StandardScaler.{0}.pkl".format(channel))
 
-        # with open("{0}/StandardScaler.{1}.pkl".format(models_folder,channel), 'wb') as FSO:
-        with open(file_manager.get_scaler_path(), 'wb') as FSO:
+        with open(file_manager.get_scaler_filepath(), 'wb') as FSO:
             cPickle.dump(scaler, FSO , 2)
         scaler = [scaler, scaler] # Hotfix since KIT uses 2 scalers
 
         trainSet = applyScaler(scaler, trainSet, variables)
 
-        model = modelObject( parameter_file = parameters,
+        model = modelObject( parameter_file=parameters,
                              variables=variables,
-                             target_names = target_names )
-        model.train( trainSet )
-        model.save(file_manager.get_model_path())
+                             target_names=target_names)
+        model.train(trainSet)
+        model.save(file_manager.get_model_filepath())
 
-    # elif not datacard:
-    #     # TODO: Maybe not needed to check. Just load what is there
-    #     if os.path.exists("{0}/StandardScaler.{1}.pkl".format(models_folder,channel) ):
-    #         print "Loading Scaler"
-    #         scaler = []
-    #         if glob("{0}/{1}_*_keras_preprocessing.pickle".format(models_folder,channel)) :
-    #             with open( "{0}/{1}_fold0_keras_preprocessing.pickle".format(models_folder,channel), "rb" ) as FSO:
-    #                 scaler.append( cPickle.load( FSO ) )
-    #
-    #             with open( "{0}/{1}_fold1_keras_preprocessing.pickle".format(models_folder,channel), "rb" ) as FSO:
-    #                 scaler.append( cPickle.load( FSO ) )
-    #         else:
-    #             with open( "{0}/StandardScaler.{1}.pkl".format(models_folder,channel), "rb" ) as FSO:
-    #                 tmp = cPickle.load( FSO )
-    #                 scaler = [tmp,tmp]
-    #
-    #
-    #     print "Loading model and predicting."
-    #     model = modelObject( filename = modelname )
-    #     read.variables = model.variables
-    #     variables = model.variables
+    elif predict:
+        if os.path.exists(file_manager.get_scaler_filepath()):
+            print "Loading Scaler"
+            with open(file_manager.get_scaler_filepath(), "rb") as FSO:
+                tmp = cPickle.load(FSO)
+                scaler = [tmp, tmp]
+        else:
+            print "Fatal: Scaler file not found at {0}. Train model using -t first.".format(file_manager.get_scaler_filepath())
+            return
 
-    # if not datacard:
-    #
-    #     outpath = read.config["outpath"] + "/predictions_" + era
-    #     predictions = {}
-    #     print "Predicting samples"
-    #     if add_nominal:
-    #         print "Predicting Nominal"
-    #         for sample, sampleConfig in read.get(what = "nominal", for_prediction = True):
-    #             sandbox(channel, model, scaler, sample, variables, "nom_" + sampleConfig["histname"], outpath ,sampleConfig, read.modifyDF )
-    #
-    #
-    #     for sample, sampleConfig in read.get(what = "full", add_jec = not short, for_prediction = True):
-    #         if "data" in sampleConfig["histname"]:
-    #             sandbox(channel, model, scaler, sample, variables, "NOMINAL_ntuple_Data", outpath, sampleConfig, read.modifyDF)
-    #         elif "full" in sampleConfig["histname"]:
-    #             sandbox(channel, model, scaler, sample, variables,  "NOMINAL_ntuple_" + sampleConfig["histname"].split("_")[0], outpath, sampleConfig, read.modifyDF )
-    #         else:
-    #             splName = sampleConfig["histname"].split("_")
-    #             sandbox(channel, model, scaler, sample, variables,  "_".join(splName[1:])+"_ntuple_" + sampleConfig["histname"].split("_")[0], outpath, sampleConfig, read.modifyDF )
-    #
-    #     if not short:
-    #         print "Predicting shapes"
-    #         for sample, sampleConfig in read.get(what = "tes", for_prediction = True):
-    #             sandbox(channel, model, scaler, sample, variables, sampleConfig["histname"], outpath ,sampleConfig, read.modifyDF )
+        print "Loading model and predicting."
+        model = modelObject(filename=file_manager.get_model_filepath())
+        read.variables = model.variables
+        variables = model.variables
+
+    if predict:
+        prediction_dir = "predictions_" + era
+        file_manager.set_prediction_dirname(prediction_dir)
+        outpath = file_manager.get_prediction_dirpath()
+
+        print "Predicting samples"
+        if add_nominal:
+            print "Predicting Nominal"
+            for sample, sampleConfig in read.get(what = "nominal", for_prediction = True):
+                sandbox(channel, model, scaler, sample, variables, "nom_" + sampleConfig["histname"], outpath ,sampleConfig, read.modifyDF )
+
+
+        for sample, sampleConfig in read.get(what = "full", add_jec = shapes, for_prediction = True):
+            if "data" in sampleConfig["histname"]:
+                sandbox(channel, model, scaler, sample, variables, "NOMINAL_ntuple_Data", outpath, sampleConfig, read.modifyDF)
+            elif "full" in sampleConfig["histname"]:
+                print "predicting for " + sampleConfig["histname"]
+                sandbox(channel, model, scaler, sample, variables,  "NOMINAL_ntuple_" + sampleConfig["histname"].split("_")[0], outpath, sampleConfig, read.modifyDF )
+            # else:
+            #     splName = sampleConfig["histname"].split("_")
+            #     sandbox(channel, model, scaler, sample, variables,  "_".join(splName[1:])+"_ntuple_" + sampleConfig["histname"].split("_")[0], outpath, sampleConfig, read.modifyDF )
+
+        # if shapes:
+        #     print "Predicting shapes"
+        #     for sample, sampleConfig in read.get(what = "tes", for_prediction = True):
+        #         sandbox(channel, model, scaler, sample, variables, sampleConfig["histname"], outpath ,sampleConfig, read.modifyDF )
 
 
 
@@ -151,19 +148,34 @@ def run(samples,channel, era, use, train,short, datacard = False, add_nominal=Fa
     #     from Tools.CutObject.CutObject import Cut
     #     from Tools.FakeFactor.FakeFactor import FakeFactor
     #
-    #     Datacard.use_config = era + "/datacard_conf"
+    #     datacard_configpath = era + "/datacard_conf"
+    #     datacard_outpath = era+"/"+use
+    #     datacard_plotpath = era+"/plots"
+    #
+    #     #file_manager.set_datacard_config_dirname(era + "/datacard_conf")
+    #
+    #     Datacard.use_config = datacard_configpath
+    #
+    #     #Datacard.use_config = file_manager.get_datacard_config_dirpath()
+    #     #Datacard.use_config = file_manager.get_datacard_config_dirname()
     #     D = Datacard(channel=channel,
     #                  variable="predicted_prob",
     #                  era=era,
     #                  real_est="mc",
-    #                  add_systematics = not short,
+    #                  add_systematics = shapes,
     #                  debug=True,
     #                  use_cutfile = "conf/cuts_{0}.json".format(era))
     #
-    #     FakeFactor.fractions = "{0}/datacard_conf/fractions/htt_ff_fractions_{0}.root".format(era)
+    #     ffpath = "{0}/datacard_conf/fractions/htt_ff_fractions_{0}.root".format(era)
+    #     #file_manager.set_fractions_filepath("../output/" + ffpath)
     #
-    #     D.create(era+"/"+use)
-    #     makePlot(channel, "ML", era+"/"+use, era, era+"/plots")
+    #     FakeFactor.fractions = ffpath
+    #
+    #     D.create(file_manager.get_datacard_output_dirname())
+    #
+    #     datacard_plotpath
+    #
+    #     makePlot(channel, "ML", datacard_outpath, era, datacard_plotpath)
 
 def sandbox(channel, model, scaler, sample, variables, outname, outpath, config = None, modify = None):
     # needed because of memory management
