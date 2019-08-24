9ccc835b6cf4928c8f1ebbd7875e1adb55ceea05
msajatov <e1125768@student.tuwien.ac.at>
2019-08-24 21:52:15 +0200

    Clean up source dir

--- source/runscripts/make_datacard.sh
+++
@@ -1,6 +0,0 @@
-ERA=$1
-CHANNEL=$2
-
-eval `scramv1 runtime -sh`
-export PYTHONPATH=$HOME/.local/lib/python2.7/site-packages:$PYTHONPATH
-python run_model.py -c ${CHANNEL} -s -e ${ERA}
--- source/runscripts/run_training.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH -J predict
-#SBATCH -D /afs/hephy.at/work/m/mspanring/CMSSW_9_4_0/src/mlFramework
-#SBATCH -o log_%j.txt
-
-python run_model.py -c $1 
